4711a7b33f78db27058ebf72bbd234b0f6fc0b65
Alexey Fosler <alexey.fosler@mail.ru>
2024-09-13 09:53:02 +0300

    feat: add command info

--- app/routers/commands/base.py
+++ app/routers/commands/base.py
@@ -12,6 +12,13 @@ async def handle_start(message: types.Message):
     )
 
 
+@router.message(Command('info'))
+async def handle_info(message: types.Message):
+    await message.answer(
+        text=f'Hello, {markdown.hbold(message.from_user.full_name)}!\nThis command is under development.'
+    )
+
+
 @router.message(Command('help'))
 async def handle_help(message: types.Message):
     await message.answer(
