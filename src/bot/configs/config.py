e5aae18704fce469da9b7a9fc314cb68f90bbb92
Alexey Fosler <alexey.fosler@mail.ru>
2024-11-22 08:45:26 +0300

    feat: fix config

--- src/bot/configs/config.py
+++ src/bot/configs/config.py
@@ -1,6 +1,19 @@
 from logging import INFO
 
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PostgresSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='POSTGRES_')
+
+    DSN: PostgresDsn = PostgresDsn(
+        'postgresql+asyncpg://admin:admin@tracker_price_robot_postgres:5432/tracker_price_robot_postgres'
+    )
+    POOL_SIZE: int = 20
+    MAX_OVERFLOW: int = 5
+    MAX_TIME: int = 2
+    MAX_TRIES: int = 3
 
 
 class Settings(BaseSettings):
@@ -12,6 +25,8 @@ class Settings(BaseSettings):
 
     bot_token: str = 'bot_token'
     logging_level: int = INFO
+
+    postgres: PostgresSettings = PostgresSettings()
 
 
 settings = Settings()
